30dc61b519beb8b54cb38e5e8164993244064241
partialarts <145933508+jemlawX@users.noreply.github.com>
2023-11-12 15:04:04 +0000

    add psuedo code

--- script/script.js
+++ script/script.js
@@ -1,4 +1,17 @@
-const currentDate = dayjs();
-const formattedDate = currentDate.format('YYYY-MM-DD HH:mm:ss');
+var currentDate = dayjs();
+var formattedDate = currentDate.format('YYYY-MM-DD HH:mm:ss');
 console.log(formattedDate);
+
+// var currentDate = getElementFromID.currentDay
+
+//Potentially get calendar elements from bootstrap
+
+// calendar elements are all form elemnents (use previous days activities and pilfer code)
+// If statement for conditional CSS in calendar linked to dayjs current date/time
+// if in the past turn grey
+// in in the future turn green
+// else red
+
+// Event listener for save buttom
+// use JSON to store events in local storage when save button is clicked
 
