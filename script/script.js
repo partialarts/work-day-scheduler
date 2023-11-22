79fc715c43c299fbb927bd996b1a701cd7c60844
partialarts <145933508+jemlawX@users.noreply.github.com>
2023-11-22 14:16:13 +0000

    Add clear calendar button to clear localStorage and reload page

--- script/script.js
+++ script/script.js
@@ -1,7 +1,15 @@
-var currentDate = $("#currentDay"); // Create ID to store the current date
+// Variable for the date and time
+var currentDate = $("#currentDay");
 var formattedDate = dayjs().format("dddd, D MMMM"); // Format the date using dayjs
 var currentHour = dayjs().format("H"); // Get current hour of the day using dayjs
 currentDate.text(formattedDate) // Display the date as text in the #currentDay ID
+
+// Variable for the clear button
+var clearBtn = $("<button>")
+clearBtn.addClass("btn mt-4"); // Adds class of btn
+clearBtn.attr("id", "clearBtn"); // Adds ID of clear to button
+$(clearBtn).text("Clear Calendar") // Apply text to clear button
+$("header").append(clearBtn) // Append to header
 
 // Function to create calendar elements, conditionally format them and append them to the document
 var generateCalendar = function () {
@@ -10,7 +18,7 @@ var generateCalendar = function () {
         var hourText = $("<div>"); // Variable for the hour column
         var hourTextInput = $("<textarea>"); // Variable for the text input
         var saveBtn = $("<button>") // Variable for the save button elements
-        var saveIcn = $("<i>") // Variable for the icon node
+        var saveIcn = $("<i>") // Variable for the icon element
         hourBlock.addClass("row time-block"); // Set classes to main row container
         hourBlock.attr("id", "hour" + i); // Set ID of hour+i so each container has unique attribute
         hourText.addClass("col-md-1 hour py-2") // Set bootstrap class for first column with padding
@@ -21,7 +29,7 @@ var generateCalendar = function () {
 
         if (i < 12) { // Conditional to set text for each hour and validate for AM and PM
             hourText.text(i + "AM"); // Apply AM to hours less than 23
-        } else if (i === 12) { 
+        } else if (i === 12) {
             hourText.text(i + "PM"); // Apply PM to 12
         } else {
             hourText.text(i - 12 + "PM"); // Converts 24 hour time to 12 hour and applies PM label
@@ -45,18 +53,25 @@ var generateCalendar = function () {
 generateCalendar(); // Call function to generate calendar elements
 
 // Waits for all HTML content before running the code within
-$(function() {
+$(function () {
 
     // Function to loop over all hour blocks and get data from the local storage
-    $(".hour").each(function () { 
+    $(".hour").each(function () {
         var thisHour = JSON.parse(localStorage.getItem($(this).text())); // Variable to return key/value pairs from localStorage and store as object
         if (thisHour !== null) { // Checks if key/value pair exists on each iteration
             $(this).siblings("textarea").val(thisHour); // Adds the the value from the object to the sibling textarea field
         }
     });
 
-    $(".saveBtn").on("click", function (event) { // Listens for click on any element with .saveBtn class
+    // Listens for click on any element with #clearBtn ID
+    $("#clearBtn").on("click", function (event) {
+        localStorage.clear(); // Clears local storage on click
+        location.reload(); // Reloads the page after storage has been cleared
+    });
+
+    // Listens for click on any element with .saveBtn class
+    $(".saveBtn").on("click", function (event) {
         event.preventDefault(); // Prevents default behaviour
         localStorage.setItem($(this).siblings(".hour").text(), JSON.stringify($(this).siblings("textarea").val())); // Sets Hour text and siblings text input to local storage as string
-      });
+    });
 }); 
